393ebf2494426ac9a4418bb552480bcc04256112
giulianacntrs <giulianacanteros15@gmail.com>
2025-11-26 19:29:00 -0300

    Se agregó el carrusel y se modifico el css

--- js/script.js
+++ js/script.js
@@ -8,3 +8,27 @@ const toggle = () => {
 
 hamburgerBtn.addEventListener("click", toggle);
 closeIcon.addEventListener("click", toggle);
+
+/* CARRUSEL */
+const images = document.querySelectorAll('.carousel-img');
+let index = 0;
+
+function showImage(newIndex) {
+  images[index].classList.remove('active');
+  index = newIndex;
+  images[index].classList.add('active');
+}
+
+document.querySelectorAll('.arrow.left').forEach(btn => {
+  btn.addEventListener('click', () => {
+    let newIndex = (index - 1 + images.length) % images.length;
+    showImage(newIndex);
+  });
+});
+
+document.querySelectorAll('.arrow.right').forEach(btn => {
+  btn.addEventListener('click', () => {
+    let newIndex = (index + 1) % images.length;
+    showImage(newIndex);
+  });
+});
