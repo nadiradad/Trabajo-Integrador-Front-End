7e8ff6264f40045fd14a2c9905c61a884622d0d8
giulianacntrs <giulianacanteros15@gmail.com>
2025-11-26 01:07:28 -0300

    Incorporación de carrusel

--- js/script.js
+++ js/script.js
@@ -8,3 +8,24 @@ const toggle = () => {
 
 hamburgerBtn.addEventListener("click", toggle);
 closeIcon.addEventListener("click", toggle);
+/* CARRUSEL */
+const images = document.querySelectorAll('.carousel-img');
+let current = 0;
+
+function showImage(index) {
+  images.forEach(img => img.classList.remove('active'));
+  images[index].classList.add('active');
+}
+
+function nextImage() {
+  current = (current + 1) % images.length;
+  showImage(current);
+}
+
+function prevImage() {
+  current = (current - 1 + images.length) % images.length;
+  showImage(current);
+}
+
+document.querySelector('.arrow.right').addEventListener('click', nextImage);
+document.querySelector('.arrow.left').addEventListener('click', prevImage);
